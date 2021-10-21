7c0e7ff47314847825783e3ff2b688b82cdefe65
Salvo Miosi <salvo.ilmiosi@gmail.com>
2021-10-21 23:43:30 +0200

    bugfix make_dir ricorsivo

--- sys_windows.h
+++ sys_windows.h
@@ -20,7 +20,7 @@ static void launch_process(const char *filename) {
 static const char *get_bin_path() {
     static char path[MAX_PATH];
     if (SUCCEEDED(SHGetFolderPathA(NULL, CSIDL_APPDATA, NULL, 0, path))) {
-        PathAppendA(path, "bang-sdl/bin");
+        PathAppendA(path, "bang-sdl\\bin");
         return path;
     }
     return NULL;
@@ -34,7 +34,19 @@ static const char *concat_path(const char *dir, const char *filename) {
 }
 
 static void make_dir(const char *filename) {
-    CreateDirectoryA(filename, NULL);
+    DWORD file_attrs = GetFileAttributesA(filename);
+    if (file_attrs == INVALID_FILE_ATTRIBUTES) {
+        char *last_slash_pos = strrchr(filename, '\\');
+        if (last_slash_pos) {
+            char sub_path[MAX_PATH];
+            strncpy(sub_path, filename, last_slash_pos - filename);
+            sub_path[last_slash_pos - filename] = '\0';
+
+            make_dir(sub_path);
+        }
+
+        CreateDirectoryA(filename, NULL);
+    }
 }
 
 static int file_exists(const char *filename) {
